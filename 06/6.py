5d6a8108abadcb17257db3b9c304aa3f50fa336d
Jack <jacksdoormat@gmail.com>
2022-12-06 09:40:28 +0000

    day 6 ⭐⭐

--- 06/6.py
+++ 06/6.py
@@ -1,18 +1,46 @@
 import init
 
-data = init.read_data(isTest=True, )
-
-
+data = init.read_data(isTest=False, )
 
 
 
 
 def part1():
-	return False
+	for line in data:
+		print(line)
+		seen_characters = []
+		for iter, value in enumerate(line):
+			if value in seen_characters:
+				index = seen_characters.index(value)
+				print('need to remove to index', index, 'as have seen', value, 'in', seen_characters)
+				seen_characters = seen_characters[index+1:]
+				print('post removal, have now seen', seen_characters)
+			seen_characters.append(value)
+			print('now seen', seen_characters)
+			if len(seen_characters) == 4:
+				print('final characters', seen_characters)
+				print('size is', iter+1)
+				break
+	return iter + 1
 
 
 def part2():
-	return False
+	for line in data:
+		print(line)
+		seen_characters = []
+		for iter, value in enumerate(line):
+			if value in seen_characters:
+				index = seen_characters.index(value)
+				print('need to remove to index', index, 'as have seen', value, 'in', seen_characters)
+				seen_characters = seen_characters[index + 1:]
+				print('post removal, have now seen', seen_characters)
+			seen_characters.append(value)
+			print('now seen', seen_characters)
+			if len(seen_characters) == 14:
+				print('final characters', seen_characters)
+				print('size is', iter + 1)
+				break
+	return iter + 1
 
 
 print(f'Part 1: {part1()}, Part 2: {part2()}')
